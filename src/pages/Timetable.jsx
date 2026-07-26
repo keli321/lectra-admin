24b8f5d84d67d7062b297f184ba5131be16ef1e7
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-07-26 20:03:19 +0100

    Created preliminary structure of timetable which gets data from a mock data

--- src/pages/Timetable.jsx
+++ src/pages/Timetable.jsx
@@ -1,12 +1,65 @@
-
+import { useState } from "react";
+import { tableData, testData } from "../utils/testData";
 
 
 function Timetable() {
-  return (
-  
-      <h1>Timetable </h1>
-     
-  );
+
+    const maxPages = Math.ceil(tableData.length / 20);
+    const [pageNumber, setPageNumber] = useState(1);
+    const initial = (pageNumber - 1) * 20;
+    const end = initial + 20;
+
+    function nextPage() {
+        setPageNumber(prev => {
+            if (prev === maxPages) return prev;
+            return prev + 1;
+        });
+    }
+
+    function previousPage() {
+        setPageNumber(prev => {
+            if (prev === 1) return prev;
+            return prev - 1;
+        });
+    }
+
+    return (
+        <section className="">
+            <table className="w-full text-left">
+                <thead className="w-full bg-[#000] text-[#FFF]">
+                    <tr className="">
+                        <th className="p-4">Date</th>
+                        <th className="p-4">Time</th>
+                        <th className="p-4">Course</th>
+                        <th className="p-4">Venue</th>
+                        <th className="p-4">Department</th>
+                    </tr>
+                </thead>
+                <tbody className="w-full">
+                    {tableData.sort((a, b) => new Date(a.date) - new Date(b.date))
+                        .slice(initial, end).map(data => (
+                            <tr className="">
+                                <td className="p-4">{data.date}</td>
+                                <td className="p-4">{data.time}</td>
+                                <td className="p-4">{data.course}</td>
+                                <td className="p-4">{data.venue}</td>
+                                <td className="p-4">{data.department}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+            <div className="h-10 flex items-center justify-between">
+                Go to next page
+                <span className="mx-5 flex gap-5 items-center">
+                    <button className="bg-[gray] py-2 px-3 cursor-pointer rounded"
+                        onClick={previousPage}>&lt;</button>
+                    {pageNumber}
+                    <button className="bg-[gray] py-2 px-3 cursor-pointer rounded"
+                        onClick={nextPage}>&gt;</button>
+                </span>
+            </div>
+        </section>
+    );
 }
 
 export default Timetable;
--- src/utils/testData.js
+++ src/utils/testData.js
@@ -41,3 +41,322 @@ export const testData = [
         note: "Require your attention"
     }
 ]
+
+export const tableData = [
+    {
+        date: "2026-10-19",
+        time: "08:00AM",
+        course: "MTH 101",
+        venue: "NB Room 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-19",
+        time: "10:00AM",
+        course: "COM 211",
+        venue: "NB Room 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-19",
+        time: "12:00PM",
+        course: "COM 214",
+        venue: "NB Room 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-19",
+        time: "02:00PM",
+        course: "COM 219",
+        venue: "NB Room 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "08:00AM",
+        course: "COM 111",
+        venue: "Lab 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "10:00AM",
+        course: "STA 112",
+        venue: "NB Room 1",
+        department: "Statistics"
+    },
+    {
+        date: "2026-10-20",
+        time: "12:00PM",
+        course: "COM 212",
+        venue: "Lab 3",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "02:00PM",
+        course: "GNS 101",
+        venue: "Main Auditorium",
+        department: "General Studies"
+    },
+    {
+        date: "2026-10-21",
+        time: "04:00PM",
+        course: "COM 215",
+        venue: "NB Room 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-21",
+        time: "08:00AM",
+        course: "MTH 201",
+        venue: "NB Room 3",
+        department: "Mathematics"
+    },
+    {
+        date: "2026-10-21",
+        time: "10:00AM",
+        course: "COM 113",
+        venue: "Lab 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-21",
+        time: "12:00PM",
+        course: "STA 211",
+        venue: "NB Room 1",
+        department: "Statistics"
+    },
+    {
+        date: "2026-10-22",
+        time: "02:00PM",
+        course: "COM 216",
+        venue: "NB Room 4",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-22",
+        time: "04:00PM",
+        course: "GNS 202",
+        venue: "Main Auditorium",
+        department: "General Studies"
+    },
+    {
+        date: "2026-10-22",
+        time: "08:00AM",
+        course: "COM 115",
+        venue: "Lab 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-22",
+        time: "10:00AM",
+        course: "MTH 102",
+        venue: "NB Room 2",
+        department: "Mathematics"
+    },
+    {
+        date: "2026-10-23",
+        time: "12:00PM",
+        course: "COM 218",
+        venue: "Lab 3",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-23",
+        time: "02:00PM",
+        course: "EET 111",
+        venue: "Electronics Lab",
+        department: "Electrical Engineering"
+    },
+    {
+        date: "2026-10-23",
+        time: "04:00PM",
+        course: "COM 112",
+        venue: "NB Room 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-23",
+        time: "08:00AM",
+        course: "COM 221",
+        venue: "Lab 2",
+        department: "Computer Science"
+    },
+    // --- 20 New Additions Below ---
+    {
+        date: "2026-10-19",
+        time: "04:00PM",
+        course: "COM 311",
+        venue: "Lab 4",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-19",
+        time: "08:00AM",
+        course: "PHY 111",
+        venue: "Physics Lab",
+        department: "Science Laboratory Technology"
+    },
+    {
+        date: "2026-10-20",
+        time: "10:00AM",
+        course: "COM 312",
+        venue: "NB Room 3",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "12:00PM",
+        course: "MTH 212",
+        venue: "NB Room 4",
+        department: "Mathematics"
+    },
+    {
+        date: "2026-10-20",
+        time: "04:00PM",
+        course: "COM 411",
+        venue: "Lab 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-21",
+        time: "08:00AM",
+        course: "CHM 111",
+        venue: "Chemistry Lab",
+        department: "Science Laboratory Technology"
+    },
+    {
+        date: "2026-10-21",
+        time: "10:00AM",
+        course: "COM 315",
+        venue: "Lab 4",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-21",
+        time: "02:00PM",
+        course: "BAM 111",
+        venue: "Management Hall",
+        department: "Business Administration"
+    },
+    {
+        date: "2026-10-22",
+        time: "10:00AM",
+        course: "COM 413",
+        venue: "Lab 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-22",
+        time: "12:00PM",
+        course: "STA 311",
+        venue: "NB Room 3",
+        department: "Statistics"
+    },
+    {
+        date: "2026-10-22",
+        time: "02:00PM",
+        course: "COM 317",
+        venue: "NB Room 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-23",
+        time: "08:00AM",
+        course: "ACC 111",
+        venue: "Management Hall",
+        department: "Accountancy"
+    },
+    {
+        date: "2026-10-23",
+        time: "10:00AM",
+        course: "COM 415",
+        venue: "Lab 4",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-26", // Next Monday
+        time: "08:00AM",
+        course: "COM 321",
+        venue: "Lab 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-26",
+        time: "10:00AM",
+        course: "MTH 301",
+        venue: "NB Room 2",
+        department: "Mathematics"
+    },
+    {
+        date: "2026-10-26",
+        time: "12:00PM",
+        course: "COM 422",
+        venue: "Lab 3",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-27", // Next Tuesday
+        time: "08:00AM",
+        course: "STA 314",
+        venue: "NB Room 4",
+        department: "Statistics"
+    },
+    {
+        date: "2026-10-27",
+        time: "10:00AM",
+        course: "COM 325",
+        venue: "Lab 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-27",
+        time: "02:00PM",
+        course: "GNS 301",
+        venue: "Main Auditorium",
+        department: "General Studies"
+    },
+    {
+        date: "2026-10-27",
+        time: "04:00PM",
+        course: "COM 426",
+        venue: "Lab 4",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-19",
+        time: "02:00PM",
+        course: "COM 219",
+        venue: "NB Room 2",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "08:00AM",
+        course: "COM 111",
+        venue: "Lab 1",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "10:00AM",
+        course: "STA 112",
+        venue: "NB Room 1",
+        department: "Statistics"
+    },
+    {
+        date: "2026-10-20",
+        time: "12:00PM",
+        course: "COM 212",
+        venue: "Lab 3",
+        department: "Computer Science"
+    },
+    {
+        date: "2026-10-20",
+        time: "02:00PM",
+        course: "GNS 101",
+        venue: "Main Auditorium",
+        department: "General Studies"
+    },
+];
