a96dfcb777edf6f89d657d25cd799004d9758147
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-08-05 01:12:59 +0100

    Additions to the pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,10 +1,15 @@
-export default function Pagination({lastPage}) {
+export default function Pagination({ reducePage, addPage, goToPage, currentPage, lastPage }) {
     return (
         <div className="justify-self-center flex gap-2 text-white mt-6 font-semibold">
-            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer">&lt;</button>
-            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer">1</button>
-            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer">{lastPage}</button>
-            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer">&gt;</button>
+            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer"
+                onClick={reducePage}>&lt;</button>
+            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer"
+                onClick={goToPage}>1</button>
+            <span className="text-black">{currentPage}</span>
+            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer"
+                onClick={goToPage}>{lastPage}</button>
+            <button className="bg-gray-500 w-6 flex justify-center cursor-pointer"
+                onClick={addPage}>&gt;</button>
         </div>
     )
 }
