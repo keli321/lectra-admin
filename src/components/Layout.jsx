6bb3a67224de8e2da98cacf1d59d4db57f985090
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-08-09 00:08:28 +0100

    Edited layout on screen sizes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,13 +11,13 @@ function Layout() {
   }
 
   return (
-    <div className="flex flex-col sm:flex-row h-screen bg-slate-100">
+    <div className="flex flex-col sm:flex-row min-h-screen md:h-screen bg-slate-100">
       <Sidebar openBar={isOpen} setOpenBar={OpenBar} />
 
-      <div className="flex min-w-0 flex-1 flex-col">
+      <div className="flex min-w-0 flex-1 flex-col h-screen">
         <Navbar openBar={isOpen} setOpenBar={OpenBar} />
 
-        <main className="flex-1 min-w-0 md:overflow-y-auto p-5 sm:p-8">
+        <main className="flex-1 min-w-0 sm:overflow-y-auto scrollbar-none p-5 sm:p-8">
           <Outlet />
         </main>
       </div>
