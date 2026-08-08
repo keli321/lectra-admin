32dd4444a19a3cc2349ed9a095d622c2e20f17eb
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-08-09 00:15:06 +0100

    Remodelled DashCard

--- src/components/DashCard.jsx
+++ src/components/DashCard.jsx
@@ -1,21 +1,23 @@
-import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsis, faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function DashCard({
     title,
     value,
-    icon
+    icon,
+    text
 }) {
 
     return (
-        <div className=" flex gap-4 rounded-xl bg-white p-4 shadow-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl">
-            <div className="flex h-10 w-10 sm:h-12 sm:w-12 items-center justify-center rounded-xl bg-violet-100">
-                <FontAwesomeIcon icon={icon} className="text-lg sm:text-xl text-violet-600" />
+        <div class="bg-white p-5 rounded-xl border border-slate-200 shadow-sm flex flex-col justify-between sm:last:col-span-2 xl:last:col-span-1">
+            <div class="flex items-center justify-between">
+                <span class="text-sm font-medium text-slate-500">{title}</span>
+                <div class="p-2 bg-indigo-50 text-indigo-600 rounded-lg">
+                    <FontAwesomeIcon icon={icon} className="text-lg sm:text-xl text-violet-600" />
+                </div>
             </div>
-            <div>
-                <p className="text-sm font-medium text-gray-600">{title}</p>
-                <h2 className="mt-1 text-xl sm:text-2xl font-bold tracking-tight text-gray-900 truncate">{value}</h2>
+            <div class="mt-1">
+                <div class="text-2xl font-bold text-slate-800">{value}</div>
+                <p class="text-xs text-slate-400 mt-1">{text}</p>
             </div>
         </div>
     );
