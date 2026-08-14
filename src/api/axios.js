c3e1ff1e5d99ed7052592e8c3691619df409a329
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-08-14 16:25:41 +0100

    Typo in api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lectra-test-backend.onrender.com/api",
+  baseURL: "https://lectra-test-backend.onrender.com/api/",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
