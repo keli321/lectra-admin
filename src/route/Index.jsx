7b38fbecd2472febe62ad692c171c97e217c023d
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-08-14 15:40:46 +0100

    Needs approval for routes for password change and otp verification

--- src/route/Index.jsx
+++ src/route/Index.jsx
@@ -6,6 +6,7 @@ import ProtectedRoute from "../components/ProtectedRoute";
 
 import Login from "../pages/Login";
 import OtpVerification from "../pages/OtpVerification";
+import ForgotPassword from "../pages/ForgotPassword";
 import ChangePassword from "../pages/ChangePassword";
 import FaceVerification from "../pages/FaceVerification";
 import Dashboard from "../pages/Dashboard";
@@ -17,11 +18,25 @@ import Notifications from "../pages/Notifications";
 import Announcements from "../pages/Announcements";
 import Announcement from "../pages/Announcement";
 import NotFound from "../pages/NotFound";
+import Otp from "../pages/Otp";
+import ResetPassword from "../pages/ResetPassword";
 
 export const router = createBrowserRouter([
     {
         path: "/login",
         element: <Login />,
+    },
+    {
+        path: "/forgot-password", // WILL NEED APPROVAL
+        element: <ForgotPassword />
+    },
+    {
+        path: "/reset-password", // WILL NEED APPROVAL
+        element: <ResetPassword />
+    },
+    {
+        path: "/otp-verification", // WILL NEED APPROVAL
+        element: <Otp />
     },
     {
         path: "/verify-otp",
