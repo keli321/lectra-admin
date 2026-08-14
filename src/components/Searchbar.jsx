357e8155a3adb31aacec5b0ffe0e3be0c85f09b7
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-08-14 15:28:24 +0100

    Created Table design component for table use

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
-export default function Searchbar({ setSearchToggle, page }) {
+export default function Searchbar({ setSearchToggle, page, setSearch }) {
     return (
         <div className="">
             <div className="relative">
                 <input className="w-full border rounded-sm px-4 py-2 pr-8 max-w-50 bg-white"
-                    placeholder={`Search ${page}`} />
+                    placeholder={`Search ${page}`} onChange={setSearch} />
                 <FontAwesomeIcon icon={faMagnifyingGlass}
                     onClick={setSearchToggle}
                     className="absolute top-[30%] -translate-x-7" />
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,22 @@
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
-import DeleteBox from "./DeleteBox";
-
-export default function Table({ deleteFunc, info, id, one, two, three, four, five }) {
-
-    const [showDelete, setShowDelete] = useState(false);
+export default function Table({
+    headArray,
+    children,
+    ...props
+}) {
 
     return (
-        <>
-            <tr key={id} className="odd:bg-white even:bg-gray-50 hover:bg-gray-100">
-                <td className="px-5 py-4">{one}</td>
-                <td className="px-5 py-4">{two}</td>
-                <td className="px-5 py-4">{three}</td>
-                <td className="px-5 py-4">{four}</td>
-                {five && <td className="px-5 py-4">{five}</td>}
-                <td className="px-5 py-4">
-                    <div className="flex justify-center gap-2">
-                        <button className="rounded-md p-2 hover:bg-blue-100"><FontAwesomeIcon icon={faPencil} /></button>
-                        <button className="rounded-md p-2 hover:bg-red-100"
-                            onClick={() => setShowDelete(prev => !prev)}><FontAwesomeIcon icon={faTrash} /></button>
-                    </div>
-                </td>
-            </tr>
-            {showDelete && <DeleteBox info={info} deleteFunc={deleteFunc} closeFunc={() => setShowDelete(false)} />}
-        </>
+        <table className="w-full border-collapse bg-white shadow-sm rounded overflow-hidden">
+            {headArray &&
+                <>
+                    <thead>
+                        <tr {...props}>
+                            {headArray.map(arr => <th className="p-3">{arr}</th>)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {children}
+                    </tbody>
+                </>}
+        </table>
     )
 }
